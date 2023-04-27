ebc086fa2ccfa1b09adac9835a1e29235d70672f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-04-27 22:53:43 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""Takes a float a and a float b as arguments
-    and returns their sum as a float
-"""
+"""Basic annotations - add"""
 
 
 def add(a: float, b: float) -> float:
