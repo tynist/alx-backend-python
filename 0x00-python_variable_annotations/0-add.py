9ed5324361eb80c99d69fa1af5070bd01176b35d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-04-27 22:44:42 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-""" type-annotated function add that takes a float a and a float b"""
+"""Takes a float a and a float b as arguments
+    and returns their sum as a float
+"""
 
 
 def add(a: float, b: float) -> float:
-    """ Adding two float together"""
+    """Adds two floats"""
     return a + b
