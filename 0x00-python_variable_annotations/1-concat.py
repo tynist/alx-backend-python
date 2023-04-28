57c77ab2c121d918123a641fb2e179eddf9f428f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-04-28 02:14:12 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""type-annotated function concat that takes a string str1 and a string str2"""
+"""Function concat two strings and concatenated it"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """ concatenated two string"""
+    """Concatenates two strings"""
     return str1 + str2
