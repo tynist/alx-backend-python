97c4411a1c21e84008213cc0b8c68fd0912a46ae
Peter Ughasi <ximonak@gmail.com>
2023-05-02 21:50:15 +0000

    async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,8 +12,8 @@ async def wait_n(n: intega, max_delay: intega) -> List[float]:
     takes two integer arguments and returns a list of float values
     """
 
-	# Create a list of coroutines, each calling wait_random(max_delay)
+    # Create a list of coroutines, each calling wait_random(max_delay)
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    
-	# Await the tasks in the order that they complete
-	return [await task for task in asyncio.as_completed(tasks)]
+
+    # Await the tasks in the order that they complete
+    return [await task for task in asyncio.as_completed(tasks)]
